aa740b20d78704433167f206a465e18c11701e81
Shyam Tailor <tailortrends5@gmail.com>
2025-11-10 13:49:32 -0700

    add theme toggle

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -6,7 +6,9 @@
     <div class="navbar-start">
       <a class="btn btn-ghost text-xl">Travel Log</a>
     </div>
+
     <div class="navbar-end">
+      <AppThemeToggle />
       <a class="btn btn-accent">Sign In with Github <Icon name="tabler:brand-github" size="24" /></a>
     </div>
   </div>
---
+++ app/components/app/theme-toggle.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set(value) {
+    colorMode.preference = value ? "dark" : "light";
+  },
+});
+</script>
+
+<template>
+  <label class="swap swap-rotate mx-4">
+    <input v-model="isDark" type="checkbox">
+    <!-- sun icon -->
+    <Icon
+      name="tabler:sun"
+      class="swap-on"
+      size="24"
+    />
+    <!-- moon icon -->
+    <Icon
+      name="tabler:moon-filled"
+      class="swap-off"
+      size="24"
+    />
+  </label>
+</template>
