85ca10e8c7ca1348963f00c62e7c8233e3d168ed
Shyam Tailor <tailortrends5@gmail.com>
2025-11-11 12:18:00 -0700

    setup db # tl-14

---
+++ app/lib/db/schema/location-log.ts
@@ -0,0 +1,17 @@
+import { real } from "drizzle-orm/gel-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+import { location } from "./location";
+
+export const locationLog = sqliteTable("locationLog", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  description: text(),
+  startedAt: int().notNull(),
+  endedAt: int().notNull(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  locationId: int().notNull().references(() => location.id),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
