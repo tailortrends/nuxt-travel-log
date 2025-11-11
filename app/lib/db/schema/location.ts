d40ae1a14093b5337553f708ea8ab2f776645c31
Shyam Tailor <tailortrends5@gmail.com>
2025-11-11 12:31:01 -0700

    setup db # tl-14 (#53)

---
+++ app/lib/db/schema/location.ts
@@ -0,0 +1,13 @@
+import { real } from "drizzle-orm/gel-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const location = sqliteTable("location", {
+  id: int().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  slug: text().notNull().unique(),
+  description: text(),
+  lat: real().notNull(),
+  long: real().notNull(),
+  createdAt: int().notNull().$default(() => Date.now()),
+  updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
+});
